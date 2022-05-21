19659916b174f852563cad171b83ed465fcb8ae2
Jacob Fernandes <jacobvg12345@gmail.com>
2022-05-21 14:41:27 -0700

    Add Changes for Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { DisplayComponent } from './display/display.component';
+import { ListingsComponent } from './listings/listings.component';
+import { AuthenticatorComponent } from './authenticator/authenticator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    DisplayComponent,
+    ListingsComponent,
+    AuthenticatorComponent
   ],
   imports: [
     BrowserModule,
